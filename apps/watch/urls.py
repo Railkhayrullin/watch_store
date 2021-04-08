02b2d5698f8e88d201b7c36edd29d9311f5ee744
rail.khayrullin <rail.khayrullin@gmail.com>
2021-04-08 17:54:26 +0500

    add page "contact"

--- apps/watch/urls.py
+++ apps/watch/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('shop/', views.shop, name='shop'),
+    path('contact/', views.contact, name='contact'),
+    # path("", views.FoodsView.as_view(), name="home"),
 ]
--- apps/watch/views.py
+++ apps/watch/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from apps.pages.models import About
 from django.views.generic import ListView, DetailView
+from apps.pages.models import About, Contact
 
 
 def index(request):
@@ -7,8 +9,19 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'watch/about.html')
+    context = {}
+    page = About.objects.get(slug='about')
+    context['page'] = page
+    return render(request, 'watch/about.html', context)
+
+
+def contact(request):
+    context = {}
+    page = Contact.objects.get(slug='contact')
+    context['page'] = page
+    return render(request, 'watch/contact.html', context)
 
 
 def shop(request):
     return render(request, 'watch/shop.html')
+
