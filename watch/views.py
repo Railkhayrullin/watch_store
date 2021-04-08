408b02044884db7e7e5954239ed3071ea1e512e6
rail.khayrullin <rail.khayrullin@gmail.com>
2021-04-08 12:27:24 +0500

    added pages "about", "shop", "navbar", "footer"

--- watch/views.py
+++ watch/views.py
@@ -4,3 +4,11 @@ from django.views.generic import ListView, DetailView
 
 def index(request):
     return render(request, 'watch/index.html')
+
+
+def about(request):
+    return render(request, 'watch/about.html')
+
+
+def shop(request):
+    return render(request, 'watch/shop.html')
